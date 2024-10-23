47eb9c0a88bbaaea32af9f6d3bb39e65b1e9821d
Dubcfrito <Dubcfrito@gmail.com>
2024-10-22 19:31:38 -0700

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module groupone.sundevilbookbank {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens groupone.sundevilbookbank to javafx.fxml;
+    exports groupone.sundevilbookbank;
+}
